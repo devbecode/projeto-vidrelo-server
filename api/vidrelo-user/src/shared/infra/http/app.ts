e6d72347ef5ee74aa688f73174707a18d3c51354
Marcello Cavazza <marcellocavazzaoliveira@gmail.com>
2022-06-24 14:10:09 -0300

    implemented swagger in the api routes

--- api/vidrelo-user/src/shared/infra/http/app.ts
+++ api/vidrelo-user/src/shared/infra/http/app.ts
@@ -6,10 +6,12 @@ import 'dotenv/config';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { ormCreateConnection } from '../database';
 import { handleError } from './middlewares/error';
 import { router } from './routes/index.router';
+import swaggerDocs from './swagger.json';
 
 const app = express();
 
@@ -21,5 +23,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/v1', router);
 app.use(handleError);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export { app };
